d7960d02e86bee3be3edd1bddc2ce12d85c4b66a
miaohua897 <haijianhoubj@gmail.com>
2024-12-11 18:25:08 -0800

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -11,10 +11,34 @@ const router = express.Router();
 
 // backend/routes/api/session.js
 // ...
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+// ...
+
+// backend/routes/api/session.js
+// ...
+// backend/routes/api/session.js
+// ...
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+
+  // backend/routes/api/session.js
+// ...
 
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
@@ -48,7 +72,6 @@ router.post(
       });
     }
   );
-
 // backend/routes/api/session.js
 // ...
 
@@ -58,6 +81,28 @@ router.delete(
     (_req, res) => {
       res.clearCookie('token');
       return res.json({ message: 'success' });
+    }
+  );
+  
+  // ...
+  // backend/routes/api/session.js
+// ...
+
+// Restore session user
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
     }
   );
   
