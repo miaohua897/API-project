f295f193f9b3c4a8b608f72df2ba8e5c1e2d1792
miaohua897 <haijianhoubj@gmail.com>
2025-01-09 09:29:54 -0800

    test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
@@ -15,12 +17,16 @@ if (import.meta.env.MODE !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
